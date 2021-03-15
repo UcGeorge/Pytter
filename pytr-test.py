8b7332eeedff1e66c13fe2c3ad184da63f786ae3
UcGeorge <petegeorge20005@gmail.com>
2021-03-15 20:38:22 +0100

    incomplete

--- pytr-test.py
+++ pytr-test.py
@@ -1,18 +1,19 @@
+from widget import Widget
 from build_context import BuildContext
 from application import Application
 from html_file import HtmlFile
-from web_page import WebPage
+from pytr_widgets import *
 
 
 class Index(HtmlFile):
 
     def build(self, context: BuildContext) -> str:
-        return WebPage()
+        return Page(
+            child=Widget()
+        )
 
 
 my_app = Application(location='''D:\Documents\pyttr-test''')
-
 index = Index(file_name='index')
-contacts = Index(file_name='contacts')
 
-my_app.add_file(index, contacts)
+my_app.add_file(index)
