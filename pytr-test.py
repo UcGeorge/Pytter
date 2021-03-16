5d7303d540609386e82dbd27339cb2022cc4ec8a
UcGeorge <petegeorge20005@gmail.com>
2021-03-16 20:24:17 +0100

    Added Widgets
    - Rows
    - Columns
    - PaddedBox

--- pytr-test.py
+++ pytr-test.py
@@ -1,8 +1,10 @@
-from styles import TextTransformation
+from styles import TextAlign, TextTransformation
 from build_context import BuildContext
 from application import Application
 from html_file import HtmlFile
 from pytr_widgets import *
+
+lorem_text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
 
 class LandingPage(HtmlFile):
@@ -20,19 +22,91 @@ class LandingPage(HtmlFile):
                 )  # TextStyle
             ),  # Text
             nav_links={
-                'GitHub': 'https://github.com/UcGeorge/Pytter',
-                'Services': 'https://github.com/UcGeorge/Pytter',
-                'Clients': 'https://github.com/UcGeorge/Pytter',
-                'Contacts': 'https://github.com/UcGeorge/Pytter'
+                'GitHub': 'https://github.com/UcGeorge/Pytter'
             },
-            child=Text(
-                text='Pytter demo page',
-                style=TextStyle(
-                    color=Color.black.value,
-                    font_size=50,
-                    letter_spacing=2
-                )  # TextStyle
-            )  # Text
+            child=Column(
+                children=[
+                    Text(
+                        text='Rows and Columns',
+                        style=TextStyle(
+                            color=Color.black.value,
+                            font_size=50,
+                            letter_spacing=2
+                        )  # TextStyle
+                    ),  # Text
+                    Row(
+                        children=[
+                            Text(
+                                text=lorem_text,
+                                style=TextStyle(
+                                    color=Color.colors('#555'),
+                                    font_size=20
+                                )  # TextStyle
+                            )  # Text
+                        ]
+                    ),  # Row
+                    Row(
+                        children=[
+                            PaddedBox(
+                                child=Text(
+                                    text=lorem_text,
+                                    style=TextStyle(
+                                        color=Color.colors('#555'),
+                                        font_size=20,
+                                        text_align=TextAlign.justify.value
+                                    )  # TextStyle
+                                ),  # Text
+                                padding=Padding(
+                                    right=10
+                                )  # Padding
+                            ),  # PaddedBox
+                            PaddedBox(
+                                child=Text(
+                                    text=lorem_text,
+                                    style=TextStyle(
+                                        color=Color.colors('#555'),
+                                        font_size=20,
+                                        text_align=TextAlign.justify.value
+                                    )  # TextStyle
+                                ),  # Text
+                                padding=Padding(
+                                    left=10
+                                )  # Padding
+                            )  # PaddedBox
+                        ]  # list[Widget]
+                    ),  # Row
+                    Row(
+                        children=[
+                            PaddedBox(
+                                child=Text(
+                                    text=lorem_text,
+                                    style=TextStyle(
+                                        color=Color.colors('#555'),
+                                        font_size=20,
+                                        text_align=TextAlign.justify.value
+                                    )  # TextStyle
+                                ),  # Text
+                                padding=Padding(
+                                    right=10
+                                )  # Padding
+                            ),  # PaddedBox
+                            PaddedBox(
+                                child=Text(
+                                    text=lorem_text,
+                                    style=TextStyle(
+                                        color=Color.colors('#555'),
+                                        font_size=20,
+                                        text_align=TextAlign.justify.value
+                                    )  # TextStyle
+                                ),  # Text
+                                padding=Padding(
+                                    left=10
+                                )  # Padding
+                            )  # PaddedBox
+                        ]  # list[Widget]
+                    ),  # Row
+                ]  # list[Widget]
+            )  # Column
         )  # MenuPage
 
 
--- pytr_widgets.py
+++ pytr_widgets.py
@@ -1,4 +1,4 @@
-from styles import Color, TextStyle
+from styles import Color, Padding, TextStyle
 from typing import Dict
 from widget import Widget
 from build_context import BuildContext
@@ -28,19 +28,19 @@ class Page(Widget):
     def build(self, context: BuildContext) -> str:
 
         return f'''
-<!DOCTYPE html>
-<html lang="{self.lang}">
-<head>
-    <meta charset="{self.charset}">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{self.title}</title>
-</head>
-<body>
-    {self.child}
-</body>
-</html>
-        '''
+            <!DOCTYPE html>
+            <html lang="{self.lang}">
+            <head>
+                <meta charset="{self.charset}">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>{self.title}</title>
+            </head>
+            <body>
+                {self.child}
+            </body>
+            </html>
+                    '''
 
 
 class MenuPage(Widget):
@@ -78,111 +78,111 @@ class MenuPage(Widget):
     def build(self, context: BuildContext) -> str:
 
         return f'''
-<!DOCTYPE html>
-<html lang="{self.lang}">
-<head>
-    <meta charset="{self.charset}">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{self.title}</title>
-</head>
+            <!DOCTYPE html>
+            <html lang="{self.lang}">
+            <head>
+                <meta charset="{self.charset}">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>{self.title}</title>
+            </head>
 
-<body>
-    <div id="mySidenav" class="sidenav">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        {self.nav_links}
-    </div>
+            <body>
+                <div id="mySidenav" class="sidenav">
+                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                    {self.nav_links}
+                </div>
 
-    <!-- Use any element to open the sidenav -->
-    <span onclick="openNav()">{self.toogle}</span>
+                <!-- Use any element to open the sidenav -->
+                <span onclick="openNav()">{self.toogle}</span>
 
-    <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
-    <div id="main">
-        {self.child}
-    </div>
-    <style>
-        /* The side navigation menu */
-        
-        .sidenav {{
-            height: 100%;
-            /* 100% \Full-height */
-            width: 0;
-            /* 0 width - change this with JavaScript */
-            position: fixed;
-            /* Stay in place */
-            z-index: 1;
-            /* Stay on top */
-            top: 0;
-            /* Stay at the top */
-            left: 0;
-            background-color: {self.background_color};
-            /* Black*/
-            overflow-x: hidden;
-            /* Disable horizontal scroll */
-            padding-top: 60px;
-            /* Place content 60px from the top */
-            transition: 0.5s;
-            /* 0.5 second transition effect to slide in the sidenav */
-        }}
-        /* The navigation menu links */
-        
-        .sidenav a {{
-            padding: 8px 8px 8px 32px;
-            text-decoration: none;
-            font-size: 25px;
-            color: #818181;
-            display: block;
-            transition: 0.3s;
-        }}
-        /* When you mouse over the navigation links, change their color */
-        
-        .sidenav a:hover {{
-            color: #f1f1f1;
-        }}
-        /* Position and style the close button (top right corner) */
-        
-        .sidenav .closebtn {{
-            position: absolute;
-            top: 0;
-            right: 25px;
-            font-size: 36px;
-            margin-left: 50px;
-        }}
-        /* Style page content - use this if you want to push the page content to the right when you open the side navigation */
-        
-        #main {{
-            transition: margin-left .5s;
-            padding: 20px;
-        }}
-        /* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
-        
-        @media screen and (max-height: 450px) {{
-            .sidenav {{
-                padding-top: 15px;
-            }}
-            .sidenav a {{
-                font-size: 18px;
-            }}
-        }}
-    </style>
-    <script>
-        /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-        function openNav() {{
-            document.getElementById("mySidenav").style.width = "250px";
-            document.getElementById("main").style.marginLeft = "250px";
-            document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-        }}
+                <!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
+                <div id="main">
+                    {self.child}
+                </div>
+                <style>
+                    /* The side navigation menu */
+                    
+                    .sidenav {{
+                        height: 100%;
+                        /* 100% \Full-height */
+                        width: 0;
+                        /* 0 width - change this with JavaScript */
+                        position: fixed;
+                        /* Stay in place */
+                        z-index: 1;
+                        /* Stay on top */
+                        top: 0;
+                        /* Stay at the top */
+                        left: 0;
+                        background-color: {self.background_color};
+                        /* Black*/
+                        overflow-x: hidden;
+                        /* Disable horizontal scroll */
+                        padding-top: 60px;
+                        /* Place content 60px from the top */
+                        transition: 0.5s;
+                        /* 0.5 second transition effect to slide in the sidenav */
+                    }}
+                    /* The navigation menu links */
+                    
+                    .sidenav a {{
+                        padding: 8px 8px 8px 32px;
+                        text-decoration: none;
+                        font-size: 25px;
+                        color: #818181;
+                        display: block;
+                        transition: 0.3s;
+                    }}
+                    /* When you mouse over the navigation links, change their color */
+                    
+                    .sidenav a:hover {{
+                        color: #f1f1f1;
+                    }}
+                    /* Position and style the close button (top right corner) */
+                    
+                    .sidenav .closebtn {{
+                        position: absolute;
+                        top: 0;
+                        right: 25px;
+                        font-size: 36px;
+                        margin-left: 50px;
+                    }}
+                    /* Style page content - use this if you want to push the page content to the right when you open the side navigation */
+                    
+                    #main {{
+                        transition: margin-left .5s;
+                        padding: 20px;
+                    }}
+                    /* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
+                    
+                    @media screen and (max-height: 450px) {{
+                        .sidenav {{
+                            padding-top: 15px;
+                        }}
+                        .sidenav a {{
+                            font-size: 18px;
+                        }}
+                    }}
+                </style>
+                <script>
+                    /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+                    function openNav() {{
+                        document.getElementById("mySidenav").style.width = "250px";
+                        document.getElementById("main").style.marginLeft = "250px";
+                        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+                    }}
 
-        /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-        function closeNav() {{
-            document.getElementById("mySidenav").style.width = "0";
-            document.getElementById("main").style.marginLeft = "0";
-            document.body.style.backgroundColor = "white";
-        }}
-    </script>
-</body>
-</html>
-        '''
+                    /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+                    function closeNav() {{
+                        document.getElementById("mySidenav").style.width = "0";
+                        document.getElementById("main").style.marginLeft = "0";
+                        document.body.style.backgroundColor = "white";
+                    }}
+                </script>
+            </body>
+            </html>
+                    '''
 
 
 class Text(Widget):
@@ -199,3 +199,62 @@ class Text(Widget):
 
     def build(self, context: BuildContext) -> str:
         return f'''<p style="{self.style}">{self.text}</p>'''
+
+
+class Column(Widget):
+    def __init__(
+        self,
+        children: 'list[Widget]' = [Widget()],
+        style: TextStyle = TextStyle(),
+    ):
+        self.children = ''.join([f'<tr>{widget}</tr>' for widget in children])
+        self.style = style
+        self.context = BuildContext()
+        self.html = self.build(self.context)
+
+    def build(self, context: BuildContext) -> str:
+        return f'''
+            <table style="height:100%">
+                {self.children}
+            </table>
+        '''
+
+
+class Row(Widget):
+    def __init__(
+        self,
+        children: 'list[Widget]' = [Widget()],
+        style: TextStyle = TextStyle(),
+    ):
+        self.children = ''.join([f'<td>{widget}</td>' for widget in children])
+        self.style = style
+        self.context = BuildContext()
+        self.html = self.build(self.context)
+
+    def build(self, context: BuildContext) -> str:  # {self.style}
+        return f'''
+            <table style="width:100%">
+            <tr>
+                {self.children}
+            </tr>
+            </table>
+        '''
+
+
+class PaddedBox(Widget):
+    def __init__(
+        self,
+        child: Widget = Widget(),
+        padding: Padding = Padding(),
+    ):
+        self.child = child
+        self.padding = padding
+        self.context = BuildContext()
+        self.html = self.build(self.context)
+
+    def build(self, context: BuildContext) -> str:
+        return f'''
+            <div style="{self.padding}">
+            {self.child}
+            </div>
+        '''
